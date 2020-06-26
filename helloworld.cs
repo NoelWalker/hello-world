946cb82537f3f8614562cd283ac77a527beaf007
noelwalker <noelwalker@hotmail.com>
2020-06-26 14:08:44 +1000

    DotNet Core Hello world

--- helloworld.cs
+++ helloworld.cs
@@ -1,2 +1,12 @@
-// a comment
+// The Usual
 
+namespace HelloWorld
+{
+    class Hello 
+    {
+        static void Main (string[] args) 
+        {
+            System.Console.WriteLine("Hello C# World");
+        } // Main
+    } // Hello
+} // HelloWorld
